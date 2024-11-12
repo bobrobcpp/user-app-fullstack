48381e8635baef152130a8f35c6242ee6663586a
Rob <king.roob@gmail.com>
2024-11-12 23:22:32 +0000

    feat: add docker files

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,7 +1,7 @@
 import 'dotenv/config';
 import { defineConfig } from 'drizzle-kit';
 export default defineConfig({
-    out: './drizzle',
+    out: './lib/db/drizzle',
     schema: './lib/db/schema.ts',
     dialect: 'postgresql',
     dbCredentials: {
