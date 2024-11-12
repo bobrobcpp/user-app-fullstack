01950e0c2aa327b7384b497e3db229a20f2b5ab7
Rob <king.roob@gmail.com>
2024-11-12 08:11:13 +0000

    feat: add user schema for drizzle-kit push db creation

---
+++ lib/db/schema.ts
@@ -0,0 +1,18 @@
+import {
+    pgTable,
+    integer,
+    varchar,
+    text,
+    timestamp,
+} from 'drizzle-orm/pg-core';
+
+export const users = pgTable('users', {
+    id: integer().primaryKey().generatedAlwaysAsIdentity(),
+    name: varchar('name', { length: 100 }),
+    email: varchar('email', { length: 255 }).notNull().unique(),
+    passwordHash: text('password_hash').notNull(),
+    createdAt: timestamp('created_at').notNull().defaultNow(),
+});
+
+export type User = typeof users.$inferSelect;
+export type NewUser = typeof users.$inferInsert;
